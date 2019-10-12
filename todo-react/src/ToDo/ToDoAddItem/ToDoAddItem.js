f6b07015bcd53a113deec11798e3396eca752f5b
Erika <erikaevelynrodriguez@gmail.com>
2019-10-12 18:14:51 -0300

    Se termino funcionalidad que agrega el item. Falta traer los responsables de la API.

--- todo-react/src/ToDo/ToDoAddItem/ToDoAddItem.js
+++ todo-react/src/ToDo/ToDoAddItem/ToDoAddItem.js
@@ -10,57 +10,74 @@ export default class ToDoAddItem extends React.Component {
         // Binding
         this.addItem = this.addItem.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeSelect = this.handleChangeSelect.bind(this);
         // Estado (puede ir fuera del constructor también)
         this.state = {
-            inputTarea: ''
+            inputTarea: '',
+            responsable: ''
         }
 
     }
 
     addItem() {
-        // this.state.inputTarea
-        alert('A name was submitted: ' + this.state.inputTarea );
+        this.toDoService.postTarea( {
+            name: this.state.inputTarea,
+            isComplete: false
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });;
     }
 
     /**
      * Se actualiza el estado (inputTarea) cada vez que el usuario modifica el input
-     * @param {event} e 
+     * @param {event} e
      */
     handleChange(e) {
         this.setState({
-            inputTarea: e.target.value
+            inputTarea: e.target.value,
+        });
+    }
+
+    handleChangeSelect(e) {
+        this.setState({
+            responsable: e.target.value
         });
     }
 
 
+
     render() {
         return (
-            <form>
-                <div className="container px-lg-5">
+            <div className="container px-lg-5">
                 <div className="row">
                     <div className="col">
-                        <input type="form" className="form-control" id="inputTarea" onChange={this.handleChange} value={this.state.inputTarea}
-                               />
+                        <input type="form" className="form-control" id="inputTarea" onChange={this.handleChange}
+                               value={this.state.inputTarea}
+                        />
                         <small className="form-text text-muted">Ingresar la tarea a realizar</small>
                     </div>
                     <div className="col">
-                        <select className="form-control" id="responsable">
+                        <select className="form-control" id="responsable" onChange={this.handleChangeSelect}
+                                value={this.state.responsable}>
                             <option value="Erika">Erika</option>
                             <option value="Marcos">Marcos</option>
                             <option value="Brenda">Brenda</option>
                             <option value="Abel">Abel</option>
                             <option value="Angela">Angela</option>
                         </select>
-                        <small id="responsable" className="form-text text-muted">Seleccionar un responsable</small>
+                        <small className="form-text text-muted">Seleccionar un responsable</small>
                     </div>
                     <div className="col">
-                        <button onClick={this.addItem} className="btn btn-primary" > <i className="material-icons">
+                        <button onClick={this.addItem} className="btn btn-primary"><i className="material-icons">
                             note_add
                         </i></button>
                     </div>
                 </div>
-                </div>
-            </form>
+            </div>
         )
     }
 }
